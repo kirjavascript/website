8f59eaa8f33f198f578a3fb63f3bfd190e6b6f71
snkenjoi <snkenjoi@gmail.com>
2016-08-06 18:55:59 +0100

    Sat Aug  6 18:55:59 BST 2016

---
+++ sites/javascript.golf/components/editor/index.jsx
@@ -0,0 +1,63 @@
+/*
+    Usage: <Editor onChange={} data={} />
+*/
+
+import React from 'react';
+
+import styles from './index.scss';
+
+let editor;
+let programmaticEdit = false;
+let updateProps = false;
+let elemId = 'aceEditor-' + Math.random().toString(36).substring(7);
+
+function getValue() {
+    return editor.getValue();
+}
+
+function setValue(str) {
+    editor.setValue(str,-1);
+}
+
+class Editor extends React.Component {
+
+    componentDidMount () {
+        editor = ace.edit(elemId);
+
+        editor.$blockScrolling = Infinity;
+        editor.getSession().setUseWorker(false);
+        editor.setTheme('ace/theme/kuroir');
+        editor.getSession().setMode('ace/mode/svg');
+        editor.setOptions({fontSize: '12pt', wrap: true});
+
+        setValue(this.props.data);
+
+        editor.getSession().on('change', () => {
+
+            if (!programmaticEdit) {
+                updateProps = true;
+                if (this.props.onChange) {
+                    this.props.onChange(getValue());
+                }
+                updateProps = false;
+            }
+
+        });
+    }
+
+    componentWillReceiveProps (props) {
+
+        if (!updateProps) {
+            programmaticEdit = true; 
+            setValue(props.data);
+            programmaticEdit = false; 
+        }
+
+    }
+
+    render () {
+        return <div className="aceEditor" id={elemId} />;
+    }
+
+}
+export default Editor;
