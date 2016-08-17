89aaab74340681b02156331ff790bc12d618ab9c
Thom <thom@gatherdigital.co.uk>
2016-08-17 14:39:00 +0100

    Wed Aug 17 14:39:00 BST 2016

--- modules/smtp.js
+++ modules/smtp.js
@@ -10,53 +10,15 @@ module.exports = function(config) {
 
     let port = config.dev ? config.port.devsmtp : config.port.smtp;
 
-
-
-
-    /* Start the Mailin server. The available options are:
-     *  options = {
-     *     port: 25,
-     *     webhook: 'http://mydomain.com/mailin/incoming,
-     *     disableWebhook: false,
-     *     logFile: '/some/local/path',
-     *     logLevel: 'warn', // One of silly, info, debug, warn, error
-     *     smtpOptions: { // Set of options directly passed to simplesmtp.createServer(smtpOptions)
-     *        SMTPBanner: 'Hi from a custom Mailin instance'
-     *     }
-     *  };
-     * Here disable the webhook posting so that you can do what you want with the
-     * parsed message. */
     mailin.start({
-      port: 25,
-      disableWebhook: true // Disable the webhook posting.
+        port,
+        disableWebhook: true // Disable the webhook posting.
     });
 
     console.log('smtpd:'+port);
 
-    /* Access simplesmtp server instance. */
-    mailin.on('authorizeUser', function(connection, username, password, done) {
-
-        console.log('user')
-        done(null, true);
-    });
-
-    /* Event emitted when a connection with the Mailin smtp server is initiated. */
-    mailin.on('startMessage', function (connection) {
-      /* connection = {
-          from: 'sender@somedomain.com',
-          to: 'someaddress@yourdomain.com',
-          id: 't84h5ugf',
-          authentication: { username: null, authenticated: false, status: 'NORMAL' }
-      }
-      }; */
-      console.log(connection);
-    });
-
-    /* Event emitted after a message was received and parsed. */
     mailin.on('message', function (connection, data, content) {
-      console.log(data);
-      /* Do something useful with the parsed message here.
-       * Use parsed message `data` directly or use raw message `content`. */
+        console.log(data);
     });
 
 
