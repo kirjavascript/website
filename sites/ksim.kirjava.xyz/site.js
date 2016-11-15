30594beecaf5944f929108117da796e42e81851e
Thom <snkenjoi@gmail.com>
2016-11-15 21:15:26 +0000

    Tue Nov 15 21:15:26 GMT 2016

--- sites/ksim.kirjava.xyz/site.js
+++
@@ -1,9 +0,0 @@
-let vhost = require('vhost');
-
-module.exports = function({app, config, express, site}) {
-
-    let hostname = config.dev ? 'localhost' : site;
-
-    app.use(vhost(hostname, express.static(`sites/${site}/static`)));
-    
-}
