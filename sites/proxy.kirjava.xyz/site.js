e6420bfca4676daeefcae82b61f95228b00687d9
kirjavascript <snkenjoi@gmail.com>
2018-09-01 15:56:41 +0100

    Sat  1 Sep 15:56:41 BST 2018

--- sites/proxy.kirjava.xyz/site.js
+++ sites/proxy.kirjava.xyz/site.js
@@ -10,12 +10,12 @@ module.exports = function({app, config, express, site}) {
 
     local.use((req, res) => {
 
-        res.send(req.url.slice(1));
-        // TODO: remove this line once namecheap issue is resolved
-        return res.send('censored by namecheap.com / efirstbank.com (for now)');
-
         let url = req.url.slice(1);
 
+        // efirstbank.com and namecheap think we're phishing...?
+        if (/https:\/\/www\.efirstbank.com/.test(url)) {
+            res.status(451).send('unavailable for legal reasons');
+        }
 
         if (!url) return res.send(`
             <a href="/http://www.google.com">
