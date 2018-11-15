6bb35ed2f4328f8d0ce767d5cc728e0b3dbd71eb
kirjavascript <snkenjoi@gmail.com>
2018-11-15 21:23:16 +0000

    cleanup

--- modules/database.js
+++
@@ -1,9 +0,0 @@
-let fs = require('fs');
-let sqlite3 = require("sqlite3");
-
-// create database if it doesn't already exist
-fs.openSync('database.db','a');
-
-var database = new sqlite3.Database('database.db');
-
-module.exports = database;
