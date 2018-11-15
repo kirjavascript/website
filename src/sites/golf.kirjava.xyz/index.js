ffc9845739a634845e1d0e8ad865b0bc9e0791f8
kirjavascript <snkenjoi@gmail.com>
2018-11-15 20:47:39 +0000

    pastebin

--- src/sites/golf.kirjava.xyz/index.js
+++ src/sites/golf.kirjava.xyz/index.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const bodyParser = require('body-parser');
-// var favicon = require('serve-favicon');
-// let initdb = require('./initdb');
-// let api = require('./api');
+const favicon = require('serve-favicon');
 
 module.exports = ({type}) => ({
     type: type.VHOST,
-    init: ({ app, getStatic, getDatabase }) => {
+    init: ({ app, getStatic, getDatabase, hostname }) => {
 
         app.use('/', getStatic());
 
@@ -24,91 +22,54 @@ module.exports = ({type}) => ({
             INSERT OR REPLACE INTO snippets(route,code)
             VALUES (?,?)
         `);
+        const code = db.prepare(`
+            SELECT code FROM snippets WHERE route = ?
+        `);
+        const routes = db.prepare(`
+            SELECT route FROM snippets
+        `);
 
-        app.use('/api/save', (req, res) => {
-
+        app.post('/api/save', (req, res) => {
             const {hash, value} = req.body;
-
-            insert.run(hash, value)
-
-            database.run(query, [hash, value, true],
-                (err,data) => {
-                    if (err) res.json({err});
-                    else res.json({});
-                }
-            )
+            insert.run(hash, value);
+            res.json({});
         })
 
-        app.use('/api/load', (req, res) => {
-
+        app.post('/api/load', (req, res) => {
             let {hash} = req.body;
-
-            let query = `
-                SELECT code FROM snippets WHERE route = ?
-            `;
-
-            database.get(query, [hash],
-                (err,data) => {
-                    if (err) {
-                        res.json({err});
-                    }
-                    else if (data && data.code) {
-                        res.json({code:data.code});
-                    }
-                }
-            )
+            res.json({code:code.get(hash)});
         })
 
-        // app.use(favicon(__dirname + '/static/favicon.ico'))
+        app.use(favicon(__dirname + '/static/favicon.ico'))
 
-        // routes for ajax functions
-        // api({ local: app, database });
+        app.get('/archive', (req, res) => {
+            res.send(routes.all().map(datum=> (
+                `<a href="/${datum.route}">${datum.route}</a><br/>`
+            )).join(''));
+        });
 
-
-        // app.get('/archive', (req, res) => {
-        //     database.all('SELECT route FROM snippets', (err, data) => {
-        //         let archive = data.map(datum=> (
-        //             `<a href="/${datum.route}">${datum.route}</a><br/>`
-        //         )).join('');
-        //         res.send(archive);
-        //     });
-        // });
-
-        // let baseHTML = fs.readFileSync(`sites/${site}/static/index.html`, 'utf-8');
+        let baseHTML = fs.readFileSync( `${__dirname}/static/index.html`, 'utf-8');
 
         // // wildcard
-        // app.use(function (req, res, next) {
+        app.use((req, res, next) => {
 
-        //     // inject requested code into the document
+            // inject requested code into the document
 
-        //     if (/\/(.*?)-(.*?)-(.*?)-(.*)/.test(req.path)) {
+            if (/\/(.*?)-(.*?)-(.*?)-(.*)/.test(req.path)) {
 
-        //         database.get('SELECT code FROM snippets WHERE route = ?',
-        //             [req.path.slice(1)],
-        //             (err, data) => {
-        //                 let code;
+                let injected = baseHTML.replace('<body>',`
+                    <body>
+                        <script>
+                        __code = ${JSON.stringify(code.get(req.path.slice(1)).code).replace(/<\/script>/g,`<"+"/script>`)};
+                    </script>
+                `);
 
-        //                 if (data && data.code) {
-        //                     code = data.code;
-        //                 }
-        //                 else {
-        //                     code = 'Error loading data';
-        //                 }
-
-        //                 let injected = baseHTML.replace('<body>',`
-        //                     <body>
-        //                         <script>
-        //                         __code = ${JSON.stringify(code).replace(/<\/script>/g,`<"+"/script>`)};
-        //                     </script>
-        //                 `);
-
-        //                 res.send(injected);
-        //             })
-        //     }
-        //     else {
-        //         res.send(baseHTML);
-        //     }
-        // });
+                res.send(injected);
+            }
+            else {
+                res.send(baseHTML);
+            }
+        });
 
     },
 });
