78c0e28c7ac3769e742511d790430fd24d2afb70
kirjavascript <snkenjoi@gmail.com>
2020-02-06 19:55:52 +0000

    moved the code

--- src/sites/kirjava.xyz/static/quine.js
+++ src/sites/kirjava.xyz/static/quine.js
@@ -1,18 +1,18 @@
      q=u=>(`in     equinequ     inequinequinequinequinequi        nequinequinequinequinequinequi
    nequinequineq   uinequin     equinequinequinequinequineq       uinequinequinequinequinequineq
  uinequinequinequi nequineq     uinequinequinequinequinequin      equinequinequinequinequinequin
-equinequinequinequinequineq     uinequinequinequinequinequine     quinequinequinequinequinequine
-quinequi   nequineq uinequi     nequine   quineq  uinequinequi    nequineq  uinequi       nequin
-equineq     uinequi nequine     quinequ   inequi  nequinequineq   uinequin  equineq
-uinequi     nequine quinequ     inequin   equine  quinequinequin  equinequ  inequinequinequin
-equineq     uinequi nequine     quinequ   inequi  nequineq uinequ inequine  quinequinequinequ
-inequin     equineq uinequi     nequine   quineq  uinequin  equinequinequi  nequinequinequine
-quinequ     inequin equineq     uinequi   nequin  equinequ   inequinequine  quinequinequinequ
-inequin  equinequin equineq     uinequi   nequin  equinequ    inequinequin  equineq
-uinequin equinequin equinequ   inequine   quineq  uinequin     equinequine  quinequ       inequi
-nequinequinequinequ inequinequinequineq uinequinequinequin      equinequinequinequinequinequineq
- uinequinequinequi   nequinequinequine  quinequinequinequi       nequinequinequinequinequinequin
-   equinequinequi      nequinequineq    uinequinequinequin        equinequinequinequinequinequin
-     equinequinequi      nequi`,e='     q='+q+';q()',`ineq         uinequinequinequinequinequine
-             quine`,
+equinequinequinequinequineq     uinequinequinequinequine`,e='     q='+q+';q()',`uinequinequinequ
+inequine   quinequi nequine     quinequ   inequi  nequinequine    quinequi  nequine       quineq
+uinequi     nequine quinequ     inequin   equine  quinequinequi   nequineq  uinequi
+nequine     quinequ inequin     equineq   uinequ  inequinequineq  uinequin  equinequinequineq
+uinequi     nequine quinequ     inequin   equine  quinequi nequin equinequ  inequinequinequin
+equineq     uinequi nequine     quinequ   inequi  nequineq  uinequinequine  quinequinequinequ
+inequin     equineq uinequi     nequine   quineq  uinequin   equinequinequ  inequinequinequin
+equineq  uinequineq uinequi     nequine   quineq  uinequin    equinequineq  uinequi
+nequineq uinequineq uinequin   equinequ   inequi  nequineq     uinequinequ  inequin       equine
+quinequinequinequin equinequinequinequi nequinequinequineq      uinequinequinequinequinequinequi
+ nequinequinequine   quinequinequinequ  inequinequinequine       quinequinequinequinequinequineq
+   uinequinequine      quinequinequi    nequinequinequineq        uinequinequinequinequinequineq
+     uinequinequine      quinequin      equinequinequinequ         inequinequinequinequinequineq
+             uineq`,
               e);q()
