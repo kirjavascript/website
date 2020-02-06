62e2e6a6aa211fa39d779a0aa31a220e58a8e796
kirjavascript <snkenjoi@gmail.com>
2020-02-06 19:45:09 +0000

    quine.js

---
+++ src/sites/kirjava.xyz/static/quine.js
@@ -0,0 +1,18 @@
+     q=u=>(`in     equinequ     inequinequinequinequinequi        nequinequinequinequinequinequi
+   nequinequineq   uinequin     equinequinequinequinequineq       uinequinequinequinequinequineq
+ uinequinequinequi nequineq     uinequinequinequinequinequin      equinequinequinequinequinequin
+equinequinequinequinequineq     uinequinequinequinequinequine     quinequinequinequinequinequine
+quinequi   nequineq uinequi     nequine   quineq  uinequinequi    nequineq  uinequi       nequin
+equineq     uinequi nequine     quinequ   inequi  nequinequineq   uinequin  equineq
+uinequi     nequine quinequ     inequin   equine  quinequinequin  equinequ  inequinequinequin
+equineq     uinequi nequine     quinequ   inequi  nequineq uinequ inequine  quinequinequinequ
+inequin     equineq uinequi     nequine   quineq  uinequin  equinequinequi  nequinequinequine
+quinequ     inequin equineq     uinequi   nequin  equinequ   inequinequine  quinequinequinequ
+inequin  equinequin equineq     uinequi   nequin  equinequ    inequinequin  equineq
+uinequin equinequin equinequ   inequine   quineq  uinequin     equinequine  quinequ       inequi
+nequinequinequinequ inequinequinequineq uinequinequinequin      equinequinequinequinequinequineq
+ uinequinequinequi   nequinequinequine  quinequinequinequi       nequinequinequinequinequinequin
+   equinequinequi      nequinequineq    uinequinequinequin        equinequinequinequinequinequin
+     equinequinequi      nequi`,e='     q='+q+';q()',`ineq         uinequinequinequinequinequine
+             quine`,
+              e);q()
